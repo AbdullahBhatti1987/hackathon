4f7891cec43e8d969547c5a1051832f916ef7608
AbdullahBhatti1987 <abdullah.bhatti345@gmail.com>
2025-01-29 12:58:01 +0500

    resolve css issue

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import legacy from '@vitejs/plugin-legacy';
-import { visualizer } from 'rollup-plugin-visualizer';
 import path from "path";
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://lmsportalb.onrender.com", // Proxy only API routes
+      "/api": "https://hackathonbackend-v6i2.onrender.com", // Proxy only API routes
     },
   },
   plugins: [
     react(),
-    visualizer({
-      open: true, // Automatically open the stats in the browser
-    }),
+    tailwindcss(),
+   
   ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom']
-        }
-      }
-    },
-    chunkSizeWarningLimit: 1000 // Adjust the chunk size limit as needed
-  }
 
 })
 
